128bd828fd6dd21d722f9fe5fca0b065032b2d1d
Okke Harsta <oharsta@zilverline.com>
2025-03-14 13:35:20 +0100

    Removed unused imports

--- src/main/java/crypto/KeyStore.java
+++ src/main/java/crypto/KeyStore.java
@@ -1,16 +1,10 @@
 package crypto;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 public interface KeyStore {
 
-    String encryptAndEncode(String secret) ;
+    String encryptAndEncode(String secret);
 
-    String decodeAndDecrypt(String encodedEncryptedSecret) ;
+    String decodeAndDecrypt(String encodedEncryptedSecret);
 
     boolean isEncryptedSecret(String input);
 }
